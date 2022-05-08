7050510ed8cab7c55317321360e7a4d13b39ca4d
Ankur <ankurmishra0043@gmail.com>
2022-05-08 15:27:07 +0530

    The first change in the code base

--- Minesweepergame/app/src/main/java/com/example/minesweeper_game/MinesweeperGame.java
+++ Minesweepergame/app/src/main/java/com/example/minesweeper_game/MinesweeperGame.java
@@ -2,10 +2,13 @@ package com.example.minesweeper_game;
 
 public class MinesweeperGame {
     private MineGrid mineGrid;
-    public MinesweeperGame(int size)
+
+    public MinesweeperGame(int size,int numberBombs)
     {
         mineGrid=new MineGrid(size);
+        mineGrid.generateGrid(numberBombs);
     }
+
     public MineGrid getMineGrid()
     {
         return mineGrid;
